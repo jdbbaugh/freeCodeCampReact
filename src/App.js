f2e1080593bf9223f59153f4790657f311fdd2ad
jdbbaugh <jbaugh78@msn.com>
2019-01-20 19:40:37 -0600

    a basic counter that can add 1 to or times 2 the current shown number

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 // import Conditional from './Conditional'
-import todoList from './todoList'
-import Todoitem from './Todoitem'
-import './index.css'
+// import todoList from './todoList'
+// import Todoitem from './Todoitem'
+// import './index.css'
 
 // export default class App extends Component {
 //   constructor() {
@@ -36,74 +36,75 @@ import './index.css'
 
 
 // TODO: THIS IS MAIN TODOLIST APP
+// export default class App extends Component {
+//   constructor() {
+//     super()
+//     this.state = {
+//       todos: todoList
+//     }
+//     this.handleChange = this.handleChange.bind(this)
+//   }
+
+//   handleChange(id) {
+//     this.setState(prevState => {
+//       const updatedTodos = prevState.todos.map(oldState => {
+//         if (oldState.id === id) {
+//           oldState.completed = !oldState.completed
+//         }
+//         return oldState
+//       })
+//       return {
+//         todos: updatedTodos
+//       }
+//     })
+//     // console.log("clicked man", id)
+//   }
+//   render() {
+//     const todoItems = this.state.todos.map(todoItm =>
+//       <Todoitem key={todoItm.id} item={todoItm} handleChange={this.handleChange}/>
+//     )
+//     return (
+//       <div className="meContainer">
+//         {todoItems}
+//       </div>
+//     )
+//   }
+// }
+
 export default class App extends Component {
   constructor() {
     super()
     this.state = {
-      todos: todoList
+      count: 0
     }
-    this.handleChange = this.handleChange.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+    this.handleClick2 = this.handleClick2.bind(this)
   }
 
-  handleChange(id) {
+  handleClick() {
     this.setState(prevState => {
-      const updatedTodos = prevState.todos.map(oldState => {
-        if (oldState.id === id) {
-          oldState.completed = !oldState.completed
-        }
-        return oldState
-      })
       return {
-        todos: updatedTodos
+        count: prevState.count + 1
       }
     })
-    // console.log("clicked man", id)
   }
+  handleClick2() {
+    this.setState(prevState => {
+      return {
+        count: prevState.count * 2
+      }
+    })
+  }
+
   render() {
-    const todoItems = this.state.todos.map(todoItm =>
-      <Todoitem key={todoItm.id} item={todoItm} handleChange={this.handleChange}/>
-    )
     return (
-      <div className="meContainer">
-        {todoItems}
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.handleClick}>Change</button>
+        <br/>
+        <button onClick={this.handleClick2}>Change x 2</button>
       </div>
     )
   }
 }
 
-// export default class App extends Component {
-//   constructor() {
-//     super()
-//     this.state = {
-//       count: 0
-//     }
-//     this.handleClick = this.handleClick.bind(this)
-//     this.handleClick2 = this.handleClick2.bind(this)
-//   }
-
-//   handleClick() {
-//     this.setState(prevState => {
-//       return {
-//         count: prevState.count + 1
-//       }
-//     })
-//   }
-//   handleClick2() {
-//     this.setState(prevState => {
-//       return {
-//         count: prevState.count * 2
-//       }
-//     })
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <h1>{this.state.count}</h1>
-//         <button onClick={this.handleClick}>Change</button>
-//         <br/>
-//         <button onClick={this.handleClick2}>Change + 2</button>
-//       </div>
-//     )
-//   }
-// }
