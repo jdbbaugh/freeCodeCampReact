c6cd4439c07a621aecf731e52783383af9c17f99
jdbbaugh <jbaugh78@msn.com>
2019-01-21 13:44:56 -0600

    interesting way of handling the event.target object in handleChange

--- src/App.js
+++ src/App.js
@@ -118,16 +118,27 @@ export default class App extends Component {
     this.handleChange = this.handleChange.bind(this)
   }
   handleChange(event) {
+    const{name, value} = event.target
     this.setState({
-      [event.target.name]: event.target.value
+      [name]: value
     })
   }
   render() {
     return(
       <form>
-        <input type="text" name="firstName" placeholder="First Name" onChange={this.handleChange} />
+        <input
+          type="text"
+          value={this.state.firstName}
+          name="firstName"
+          placeholder="First Name"
+          onChange={this.handleChange} />
         <br/>
-        <input type="text" name="lastName" placeholder="Last Name" onChange={this.handleChange} />
+        <input
+          type="text"
+          value={this.state.lastName}
+          name="lastName"
+          placeholder="Last Name"
+          onChange={this.handleChange} />
         <h1>{this.state.firstName} {this.state.lastName}</h1>
       </form>
     )
